0d81ae1b7601358936d786f36a423fac12f49455
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2012-05-06 20:57:48 +0300

    Skeleton for downloader

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -1,10 +1,32 @@
 module Skyhook
 	class Downloader
-		def initialize(storage)
+		def initialize(storage, bucket_name, options = {})
 			@storage = storage
+			begin
+				bucket_info = @storage.get_bucket(bucket_name)
+				p bucket_info
+			rescue Exception => e
+				raise "No such bucket #{bucket_name}"
+			end
+		end
+		
+		def download(paths)
+			paths.each do |path|
+				if FileTest.directory? path then
+					
+				else
+				
+				end
+			end
 		end
 	
 	private
-	
+		def download_directory(path)
+		end
+		
+		def download_file(path)
+			remote_file = @storage.files.get(path)
+		end
+		
 	end
 end
