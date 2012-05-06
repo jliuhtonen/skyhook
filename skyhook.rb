f08e8b6f54a243ff793847695b5d088801fe1706
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2012-05-06 13:57:21 +0300

    requires changed

--- skyhook.rb
+++ skyhook.rb
@@ -1,8 +1,8 @@
 require 'fog'
 require 'find'
 require 'optparse'
-require './uploader.rb'
-require './cli.rb'
+require File.join(File.dirname(__FILE__), 'uploader.rb')
+require File.join(File.dirname(__FILE__), 'cli.rb')
 
 if __FILE__ == $0 then
 	Skyhook::Cli.new.run
