6f6b68503c494289ba324ce4d3462794e7754080
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2012-05-06 17:33:10 +0300

    changed magic number to constant

--- uploader.rb
+++ uploader.rb
@@ -9,10 +9,12 @@ module Skyhook
 			md5 = File.open(filepath, 'rb') do |stream|
   				digest = Digest::MD5.new
   				buffer = ""
-  				digest.update(buffer) while stream.read(4096, buffer)
+  				digest.update(buffer) while stream.read(@@BUFFER_SIZE, buffer)
   				return digest.to_s
 			end
 		end
+	private
+		@@BUFFER_SIZE = 4096
 	end
 
 	class Uploader
